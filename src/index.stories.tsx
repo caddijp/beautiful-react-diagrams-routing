758d7c3e01364ec5d89e19e7ba0b4b813223084f
Keisuke Kan <keisuke_kan@caddi.jp>
2020-10-23 17:47:28 +0900

    chore(src): initial links

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, Story } from "@storybook/react";
 import Diagram, { useSchema } from "beautiful-react-diagrams";
-import type { Node } from "beautiful-react-diagrams/@types/DiagramSchema";
+import type { Link, Node } from "beautiful-react-diagrams/@types/DiagramSchema";
 import "beautiful-react-diagrams/styles.css";
 import React, { ComponentProps } from "react";
 import { Factory } from "rosie";
@@ -12,18 +12,27 @@ const meta: Meta = {
 export default meta;
 
 const nodes = new Factory<Node<unknown>>()
-  .sequence("id", (i) => i)
+  .sequence("id", (i) => i.toString())
   .attr("coordinates", [0, 0])
-  .attr("content", ["id"], (id) => id)
+  .attr("content", ["id"], (id) => <div>{id}</div>)
   .buildList(10);
+
+const links: Link[] = [
+  { input: "1", output: "2" },
+  { input: "1", output: "3" },
+  { input: "1", output: "4" },
+];
 
 const Template: Story<ComponentProps<typeof Diagram>> = ({
   schema: initialSchema,
 }) => {
-  const [schema] = useSchema(initialSchema || { nodes: [], links: [] });
+  const [schema, { onChange }] = useSchema(
+    initialSchema || { nodes: [], links: [] }
+  );
+
   return (
     <div style={{ height: "200rem" }}>
-      <Diagram schema={schema} />
+      <Diagram schema={schema} onChange={onChange} />
     </div>
   );
 };
@@ -33,6 +42,6 @@ export const ReRoutingDiagram = Template.bind({});
 ReRoutingDiagram.args = {
   schema: {
     nodes,
-    links: [],
+    links,
   },
 };
